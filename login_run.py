02b23e28d503c7176721cacaaf11d1148683aef7
Your Name <you@example.com>
2023-07-19 13:39:32 +0800

    完成登录页面的编写

--- login_run.py
+++ login_run.py
@@ -29,21 +29,129 @@ class Form_login(QWidget,Ui_Form):
 
     def handle_button(self):
         self.pushButton.clicked.connect(self.login)
+        self.pushButton_2.clicked.connect(self.remember_password)
 
-    def showTopTip(self):
+    def remember_password(self):
+        if self.lineEdit_3.text():
+            username = self.lineEdit_3.text()
+            conn,cursor = get_conn()
+            sql = "SELECT * FROM users where username=%s"
+            cursor.execute(sql,(username))
+            data = cursor.fetchone()
+            if data:
+                self.lineEdit_4.setText(data[2])
+                self.showTopTip_success()
+            else:
+                self.showTopTip_remember_error()
+
+
+
+
+    def showTopTip_remember_error(self):
         position = TeachingTipTailPosition.BOTTOM
         view = TeachingTipView(
             icon=None,
-            title='指令错误',
-            content="最短的捷径就是绕远路，绕远路才是我的最短捷径。",
-            image='./icons/550w.bmp',
-            # image='resource/boqi.gif',
+            title='系统无法检索操作员',
+            content="人类把最精密的保密系统，都用在了自我毁灭上。",
+            image='./icons/user_error.jpeg',
             isClosable=True,
             tailPosition=position,
         )
 
         # add widget to view
         button = PushButton('Action')
+        button.setFixedWidth(120)
+        view.addWidget(button, align=Qt.AlignRight)
+
+        w = TeachingTip.make(
+            target=self.pushButton,
+            view=view,
+            duration=-1,
+            tailPosition=position,
+            parent=self
+        )
+        view.closed.connect(w.close)
+    def showTopTip_success(self):
+        position = TeachingTipTailPosition.BOTTOM
+        view = TeachingTipView(
+            icon=None,
+            title="点火成功",
+            content="你觉得不公平吗？危难当前，唯有责任。",
+            image='./icons/earth_successful.png',
+            isClosable=True,
+            tailPosition=position,
+        )
+
+        # add widget to view
+        button = PushButton('了解')
+        button.setFixedWidth(120)
+        view.addWidget(button, align=Qt.AlignRight)
+
+
+    def showTopTip(self):
+        position = TeachingTipTailPosition.BOTTOM
+        view = TeachingTipView(
+            icon=None,
+            title='指令错误',
+            content="生存的最大障碍，从不是弱小，而是傲慢。",
+            image='./icons/login_error.jpeg',
+            isClosable=True,
+            tailPosition=position,
+        )
+
+        # add widget to view
+        button = PushButton('Action')
+        button.setFixedWidth(120)
+        view.addWidget(button, align=Qt.AlignRight)
+
+        w = TeachingTip.make(
+            target=self.pushButton,
+            view=view,
+            duration=-1,
+            tailPosition=position,
+            parent=self
+        )
+        view.closed.connect(w.close)
+    def showTopTip_success(self):
+        position = TeachingTipTailPosition.BOTTOM
+        view = TeachingTipView(
+            icon=None,
+            title="点火成功",
+            content="你觉得不公平吗？危难当前，唯有责任。",
+            image='./icons/earth_successful.png',
+            # image='resource/boqi.gif',
+            isClosable=True,
+            tailPosition=position,
+        )
+
+        # add widget to view
+        button = PushButton('了解')
+        button.setFixedWidth(120)
+        view.addWidget(button, align=Qt.AlignRight)
+
+        w = TeachingTip.make(
+            target=self.pushButton_2,
+            view=view,
+            duration=-1,
+            tailPosition=position,
+            parent=self
+        )
+        view.closed.connect(w.close)
+
+    def showTopTip_mima(self):
+        position = TeachingTipTailPosition.BOTTOM
+        view = TeachingTipView(
+            icon=None,
+            title="密码错误",
+            content="生存的最大障碍，从不是弱小，而是傲慢。",
+            image='./icons/login_error.jpeg',
+            # image='resource/boqi.gif',
+            isClosable=True,
+            tailPosition=position,
+        )
+
+        # add widget to view
+        button = PushButton('了解')
         button.setFixedWidth(120)
         view.addWidget(button, align=Qt.AlignRight)
 
@@ -70,7 +178,7 @@ class Form_login(QWidget,Ui_Form):
                 self.close()
                 self.open_main_run()
             else:
-                self.showTopTip()
+                self.showTopTip_mima()
                 close_conn(conn, cursor)
         else:
             self.showTopTip()
@@ -108,10 +216,10 @@ def main():
     QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
     app = QApplication(sys.argv)
 
-    # splash = SplashScreen()
-    # splash.show()
-    # time.sleep(3)
-    # splash.close()
+    splash = SplashScreen()
+    splash.show()
+    time.sleep(3)
+    splash.close()
 
     mainwindow = Form_login()
     mainwindow.setWindowTitle("登录")
--- run.py
+++ run.py
@@ -11,10 +11,11 @@ from PyQt5.QtGui import QIcon, QDesktopServices,QPixmap,QFont
 from PyQt5.QtWidgets import QApplication, QWidget,QStatusBar,QVBoxLayout,QLabel,QMainWindow,QPushButton,QLineEdit
 from qfluentwidgets import (NavigationItemPosition, MessageBox, setTheme, Theme,
                             NavigationAvatarWidget,  SplitFluentWindow, FluentTranslator)
+from qfluentwidgets import (PushButton, Flyout, InfoBarIcon, FlyoutView, FlyoutViewBase,
+                            BodyLabel, setFont, PrimaryPushButton, FlyoutAnimationType)
 from qfluentwidgets import FluentIcon as FIF
 from enum import Enum
 from qfluentwidgets import getIconColor, Theme, FluentIconBase
-
 from utils.tools import SplashScreen,MyFluentIcon
 
 from waterinf_run import Form_waterinf
@@ -34,6 +35,7 @@ class Window(SplitFluentWindow):
         self.create_sub()
         self.initNavigation()
         self.inin_title()
+        self.create_nag()
 
         # splash = SplashScreen()
         # time.sleep(5)
@@ -43,6 +45,7 @@ class Window(SplitFluentWindow):
         # 设置窗体标题
         self.setWindowTitle("径流预报调度系统")
         self.setWindowIcon(QIcon("./icons/System_black.svg"))
+        self.theme_change_control = 1
         # 设置窗口大小
         self.resize(1000, 650)
         # 居中显示
@@ -51,7 +54,42 @@ class Window(SplitFluentWindow):
         self.move(w // 2 - self.width() // 2, h // 2 - self.height() // 2)
 
 
+    def create_nag(self):
+        self.navigationInterface.addWidget(
+            routeKey='avatar',
+            widget=NavigationAvatarWidget('Jim', './icons/user.jpg'),
+            onClick=self.inf_user,
+            position=NavigationItemPosition.BOTTOM,
+        )
+        self.navigationInterface.addWidget(
+            routeKey='settingInterface',
+            widget=NavigationAvatarWidget('设置', "./icons/theme.svg"),
+            onClick=self.change_theme,
+            position=NavigationItemPosition.BOTTOM,
+        )
 
+        self.navigationInterface.setExpandWidth(280)
+
+    def inf_user(self):
+
+        w = MessageBox(
+            '看一眼就爆炸🥰',
+            '本项目仅供学习使用，不可用于任何形式的商业用途，如果一定要商用请仔细阅读源码，排除代码里留的后门，因非法商用造成的一切后果与作者无关',
+            self
+        )
+        w.yesButton.setText('原地爆炸')
+        w.cancelButton.setText('下次一定')
+
+        if w.exec():
+            QDesktopServices.openUrl(QUrl("https://github.com/Jim2742514076/Runoff_Forecast_Dispatch_System"))
+
+    def change_theme(self):
+        if self.theme_change_control % 2 == 1:
+            self.theme_change_control +=1
+            setTheme(Theme.DARK)
+        else:
+            self.theme_change_control +=1
+            setTheme(Theme.LIGHT)
 
     def create_sub(self):
         # create sub interface
